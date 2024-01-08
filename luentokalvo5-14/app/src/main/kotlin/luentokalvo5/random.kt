a120fc33457069b6d672cd30e59fc5682b3ee682
santtuniskanen <santtuniska02@gmail.com>
2024-01-08 17:49:44 +0200

    homework number 1

---
+++ luentokalvo5-14/app/src/main/kotlin/luentokalvo5/random.kt
@@ -0,0 +1,24 @@
+package luentokalvo5
+
+fun rangeDice() {
+    val diceRange = 1..6
+    val randomNumber = diceRange.random()
+    println("Random number: ${randomNumber}")
+}
+
+fun firstDice() {
+    val myFirstDice = Dice(6)
+    val diceRoll = myFirstDice.roll()
+    println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
+        
+    val mySecondDice = Dice(20)
+    println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
+    
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        val randomNumber = (1..numSides).random()
+        return randomNumber
+    }
+}
