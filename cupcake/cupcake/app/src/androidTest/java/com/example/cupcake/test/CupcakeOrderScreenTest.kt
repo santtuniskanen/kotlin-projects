8d5a2d899c622e7aa7ed061998efe6de46fc67b9
santtuniskanen <santtuniska02@gmail.com>
2024-02-27 21:16:25 +0200

    finally fixed tests

---
+++ cupcake/cupcake/app/src/androidTest/java/com/example/cupcake/test/CupcakeOrderScreenTest.kt
@@ -0,0 +1,116 @@
+package com.example.cupcake.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.example.cupcake.R
+import com.example.cupcake.data.DataSource
+import com.example.cupcake.data.OrderUiState
+import com.example.cupcake.ui.OrderSummaryScreen
+import com.example.cupcake.ui.SelectOptionScreen
+import com.example.cupcake.ui.StartOrderScreen
+import org.junit.Rule
+import org.junit.Test
+
+class CupcakeOrderScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private val fakeOrderUiState = OrderUiState(
+        quantity = 6,
+        flavor = "Vanilla",
+        date = "Wed Jul 21",
+        price = "$100",
+        pickupOptions = listOf()
+    )
+
+    @Test
+    fun startOrderScreen_verifyContent() {
+
+        composeTestRule.setContent {
+            StartOrderScreen(
+                quantityOptions = DataSource.quantityOptions,
+                onNextButtonClicked = {}
+            )
+        }
+
+        DataSource.quantityOptions.forEach {
+            composeTestRule.onNodeWithStringId(it.first).assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun selectOptionScreen_verifyContent() {
+        // Given list of options
+        val flavors = listOf("Vanilla", "Chocolate", "Hazelnut", "Cookie", "Mango")
+        // And subtotal
+        val subtotal = "$100"
+
+        // When SelectOptionScreen is loaded
+        composeTestRule.setContent {
+            SelectOptionScreen(subtotal = subtotal, options = flavors)
+        }
+
+        // Then all the options are displayed on the screen.
+        flavors.forEach { flavor ->
+            composeTestRule.onNodeWithText(flavor).assertIsDisplayed()
+        }
+
+        // And then the subtotal is displayed correctly.
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.subtotal_price,
+                subtotal
+            )
+        ).assertIsDisplayed()
+
+        // And then the next button is disabled
+        composeTestRule.onNodeWithStringId(R.string.next).assertIsNotEnabled()
+    }
+
+    @Test
+    fun selectOptionScreen_optionSelected_NextButtonEnabled() {
+        // Given list of options
+        val flavours = listOf("Vanilla", "Chocolate", "Hazelnut", "Cookie", "Mango")
+        // And sub total
+        val subTotal = "$100"
+
+        // When SelectOptionScreen is loaded
+        composeTestRule.setContent {
+            SelectOptionScreen(subtotal = subTotal, options = flavours)
+        }
+
+        // And one option is selected
+        composeTestRule.onNodeWithText("Vanilla").performClick()
+
+        // Then the next button is disabled
+        composeTestRule.onNodeWithStringId(R.string.next).assertIsEnabled()
+    }
+
+    @Test
+    fun summaryScreen_verifyContentDisplay() {
+        // When Summary Screen is loaded
+        composeTestRule.setContent {
+            OrderSummaryScreen(
+                orderUiState = fakeOrderUiState,
+                onCancelButtonClicked = {},
+                onSendButtonClicked = { _, _ -> },
+            )
+        }
+
+        // Then the UI is updated correctly.
+        composeTestRule.onNodeWithText(fakeOrderUiState.flavor).assertIsDisplayed()
+        composeTestRule.onNodeWithText(fakeOrderUiState.date).assertIsDisplayed()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.subtotal_price,
+                fakeOrderUiState.price
+            )
+        ).assertIsDisplayed()
+    }
+}
