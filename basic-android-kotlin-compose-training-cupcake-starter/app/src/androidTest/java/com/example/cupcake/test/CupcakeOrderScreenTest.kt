fca84c599a11c7fa6831f1c438c9b6cd3acdcfd1
santtuniskanen <santtuniska02@gmail.com>
2024-02-27 20:46:40 +0200

    added order tests

---
+++ basic-android-kotlin-compose-training-cupcake-starter/app/src/androidTest/java/com/example/cupcake/test/CupcakeOrderScreenTest.kt
@@ -0,0 +1,16 @@
+package com.example.cupcake.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import org.junit.Rule
+import org.junit.Test
+
+class CupcakeOrderScreenTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun selectOptionScreen_verifyContent() {
+
+    }
+}
